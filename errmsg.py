28a3949fe9a5760191af8622ceefe742cad988a5
Cameron <cameron.hadfield@gmail.com>
2019-03-10 16:19:27 -0400

    Fixed runtime error with errmsg

--- errmsg.py
+++ errmsg.py
@@ -1,6 +1,6 @@
 #errormessages
 from random import*
-messages = ["Die immediately", "Waste of space", ">>>:(", "error no. 69", ">:(", "hmmm", "I can't believe you've done this", "Please stop I need to feed my family", "JUST DO SOMETHING RIGHT", "Me and your mother are very disappointed in you"
+messages = ["Die immediately", "Waste of space", ">>>:(", "error no. 69", ">:(", "hmmm", "I can't believe you've done this", "Please stop I need to feed my family", "JUST DO SOMETHING RIGHT", "Me and your mother are very disappointed in you"]
 def getMessage():
     num = randrange(0, len(messages))
     return messages[int(num)]
